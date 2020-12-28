ae2ef848e8661c410e879504913d70cebcec1e93
AlexPigalyov <alex.pigalyov@gmail.com>
2020-12-29 00:52:02 +0300

    add currency domain model + currency type enum

---
+++ nopCommerce_4.30_Source/Libraries/Nop.Core/Domain/Currencies/CurrencyTypeEnum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nop.Core.Domain.Currencies
+{
+    public enum CurrencyTypeEnum
+    {
+        Gold = 1,
+        Silver = 2,
+        Platinum = 3,
+        Palladium = 4,
+        Euro = 5,
+        Dollar = 6
+    }
+}
