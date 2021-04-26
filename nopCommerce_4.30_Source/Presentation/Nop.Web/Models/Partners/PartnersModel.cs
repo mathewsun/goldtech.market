b08938de6fc242f50a5c5bd6d6f1c44cbcc24a1d
Matvei Dubrovin <mathew.dubrovin@gmail.com>
2021-04-27 01:01:00 +0300

    add partners page

---
+++ nopCommerce_4.30_Source/Presentation/Nop.Web/Models/Partners/PartnersModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Nop.Web.Models.Home
+{
+    public class PartnersModel
+    {
+        public bool IsAuthetihicated { get; set; }
+        public string PartnerUrl { get; set; }
+    }
+}
